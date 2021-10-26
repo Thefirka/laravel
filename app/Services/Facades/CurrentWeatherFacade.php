756f1c9ec4a762cdd6b476e4593413bed59c565d
Thefirka <43805150+Thefirka@users.noreply.github.com>
2021-10-26 11:50:27 +0300

    Create CurrentWeatherFacade.php

---
+++ app/Services/Facades/CurrentWeatherFacade.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Services\OpenWeatherApi;
+
+
+class CurrentWeatherFacade
+{
+    protected static function getWeatherFacade()
+    {
+        return 'CurrentWeather';
+    }
+}
